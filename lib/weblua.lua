8fb6f833bd816c91f602d3dd215e42bf1f580020
Leandro Andrade <leojsandrade@gmail.com>
2011-10-20 12:13:17 -0300

    correcting weblua

--- lib/weblua.lua
+++ lib/weblua.lua
@@ -16,28 +16,34 @@ end
 function rest_request(url, paramstable, callback, format, method)
     callback_func = callback
     
-    if format ~= 'xml' or format ~= 'json' then
+    if format ~= 'xml' and format ~= 'json' then
         return false
     end
     data_format = format
     
-    if method == 'get' then
+    if method == 'get' then        
         url = url .. '?' .. util.urlEncode(paramstable)
         http.request(url, callback_handler, method)
     elseif method == 'post' then
         http.request(url, callback_handler, method, paramstable)
-    end   
-    
+    end    
 end
 
 function callback_handler(header, body)
-    print('\n\n\nentering callback2\n\n\n')
-    print(header .. '\n\n')
-    print(body .. '\n\n')
+    --print(header .. '\n\n')
+    --print(body .. '\n\n')
     if data_format == 'xml' then
         response_table = xml_to_table(body)
+        print('response_table:')
+        for k, v in pairs(response_table) do
+            print(k,":",v)
+        end
     elseif data_format == 'json' then
         response_table = json_to_table(body)
+        print('response_table:')
+        for k, v in pairs(response_table) do
+            print(k,":",v)
+        end
     elseif data_format == 'soap' then
         response_table = soap_xml_to_table(body)
         print('response_table:')
